b058725d46bc8b56285d98d9ad5f2d3d9ea0da29
DinmaOtutu <otutudinma1995@gmail.com>
2019-04-05 22:11:08 +0100

    project solution

--- src/bin/www.js
+++ src/bin/www.js
@@ -9,18 +9,6 @@ dotenv.config();
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 1200;
 
-const connectionUrl = process.env.NODE_ENV === 'test'
-  ? process.env.DB_URL_TEST : process.env.DB_URL;
-
-// connect to mongodb database
-mongoose.connect(connectionUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}, () => {
-  logger.info('Connected to database successfully');
-});
-
 app.listen(port, () => {
   logger.info(`API is listening on ${hostname}:${port}`);
 });
--- src/middlewares/verifyMealIdInputs.js
+++ src/middlewares/verifyMealIdInputs.js
@@ -2,8 +2,8 @@ import validationErrorHandler from './validationErrorHandler';
 
 const verifyMealIdInputs = {
   mealIdBody: (req, res, next) => {
-    req.check('mealId', 'meal ID is required').trim().notEmpty();
-    req.check('mealId', 'meal ID should be an array').isArray();
+    req.check('mealIds', 'meal ID is required').trim().notEmpty();
+    req.check('mealIds', 'meal ID should be an array').isArray();
 
     validationErrorHandler(req, res, next);
   }
